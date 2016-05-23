c6c3fdc836f7f510af3db2c427d8f58d675d27b2
fada21 <lukasz.czaplicki@gmail.com>
2016-05-24 00:35:48 +0100

    add contact picker from intent

--- app/src/main/java/com/fada21/android/kissendo/contact/ContactPickerFragment.java
+++ app/src/main/java/com/fada21/android/kissendo/contact/ContactPickerFragment.java
@@ -1,8 +1,10 @@
 package com.fada21.android.kissendo.contact;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.telephony.PhoneNumberUtils;
@@ -10,6 +12,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.fada21.android.kissendo.R;
@@ -17,18 +20,24 @@ import com.fada21.android.kissendo.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static android.app.Activity.RESULT_OK;
 import static com.fada21.android.kissendo.utils.Consts.PREF_NUMBER;
 
 public class ContactPickerFragment extends Fragment {
 
     public static final String TAG_ContactPickerFragment = "TAG_ContactPickerFragment";
+    private static final int CONTACT_PICKER_RESULT = 44;
 
     @BindView(R.id.edit_addressee_number) EditText phoneNumberInput;
+    @BindView(R.id.btn_contact_picker) Button contactPickerBtn;
 
     @Nullable @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_content_picker, container, false);
         ButterKnife.bind(this, view);
+
+        contactPickerBtn.setOnClickListener(v -> pickContact());
+
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         phoneNumberInput.setText(prefs.getString(PREF_NUMBER, ""));
         phoneNumberInput.setOnFocusChangeListener((v, hasFocus) -> {
@@ -37,6 +46,26 @@ public class ContactPickerFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CONTACT_PICKER_RESULT:
+                    // handle contact results
+                    break;
+            }
+
+        } else {
+            // gracefully handle failure
+        }
+    }
+
+    private void pickContact() {
+        Intent contactPickerIntent = new Intent(Intent.ACTION_PICK,
+                                                ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(contactPickerIntent, CONTACT_PICKER_RESULT);
     }
 
     private void storeNumber(final String number) {
